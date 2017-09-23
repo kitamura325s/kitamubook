8bac6a03c0e4e8d1e5626644a7fab5dfeac9d7b9
Kitamura Saori <kitamura325s@gmail.com>
2017-09-23 02:13:28 +0000

    sendgrid config mod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'warm-refuge-25483.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://13.113.216.89' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
